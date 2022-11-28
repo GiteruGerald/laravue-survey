102b05e499c5be65e4664bf6c8ce4673649489be
GerryNdungu <gerryjerry36@gmail.com>
2022-11-28 23:27:12 +0300

    added vue-router and default layout

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 // import './style.css'
 import './main.css'
 
-import App from './App.vue'
-import store from './store'
+import App from './App.vue';
+import store from './store';
+import router from './router/'
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>All surveys</h1>
+</template>
+  
+  <script setup>
+  
+  </script>
